b9d01bc62ea0f45c63bfbb2ac620902946bd03dc
Erik Gúth <guthember@gmail.com>
2019-09-17 15:12:32 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nagyobb
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int a;
+      int b;
+
+      Console.Write("First number: ");
+      a = Convert.ToInt32(Console.ReadLine());
+
+      Console.Write("Second number: ");
+      b = Convert.ToInt32(Console.ReadLine());
+
+      if (b > a)
+      {
+        Console.WriteLine("The second number is greater.");
+      }
+      else
+      {
+        Console.WriteLine("The first number is greater.");
+      }
+
+
+
+      Console.ReadKey();
+    }
+  }
+}
